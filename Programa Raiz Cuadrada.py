f38ac8552a226e9a4adb256e01078751dc68a25a
RM <ramava12594@gmail.com>
2024-02-08 00:07:11 +0100

    cuerpo

---
+++ Programa Raiz Cuadrada.py
@@ -0,0 +1,23 @@
+import math
+print("Programa de cálculo de Raíz cuadrada")
+print()
+numero=int(input("Introduce un número: "))
+
+intentos=0
+
+while numero<0:
+    print("No se puede hayar la raíz cuadrada de un nímero negativo.")
+
+    if intentos==2:
+        print("Has consumido demasiados intentos. El programa ha finalizado")
+        break;
+
+    numero=int(input("Introduce un número: "))
+    if numero<0:
+        intentos=intentos+1
+
+if intentos<2:
+    solucion=math.sqrt(numero)
+    print("La raíz cuadrada de " + str(numero) + " es: " + str(solucion))
+
+
