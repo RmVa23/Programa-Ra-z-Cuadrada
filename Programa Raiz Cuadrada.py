657619bb570c482f526dd0df6eeca391b3cb650d
RM <ramava12594@gmail.com>
2024-02-08 00:41:52 +0100

    espacios

--- Programa Raiz Cuadrada.py
+++ Programa Raiz Cuadrada.py
@@ -6,9 +6,12 @@ numero=int(input("Introduce un número: "))
 intentos=0
 
 while numero<0:
-    print("No se puede hayar la raíz cuadrada de un nímero negativo.")
+    print()
+    print("No se puede hayar la raíz cuadrada de un numero negativo.")
+    print()
 
     if intentos==2:
+        print()
         print("Has consumido demasiados intentos. El programa ha finalizado")
         break;
 
@@ -18,5 +21,7 @@ while numero<0:
 
 if intentos<2:
     solucion=math.sqrt(numero)
+    print()
     print("La raíz cuadrada de " + str(numero) + " es: " + str(solucion))
+    print()
     print("El programa ha finalizado.")
